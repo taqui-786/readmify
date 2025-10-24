99ea18558a7c03c8dbddd0abb85dccb0f07a96a5
taqui-786 <mdtaqui.jhar@gmail.com>
2025-10-24 19:53:31 +0530

    fix: removed authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import LogoutBtn from "@/components/LogoutBtn";
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth";
+
 
 export default async function Home() {
-const session = await auth.api.getSession({
-    headers: await headers() // you need to pass the headers object.
-})
-console.log({session});
+
 
   return (
-    <main>Landing Page <LogoutBtn/></main>
+    <main className=""></main>
   );
 }
